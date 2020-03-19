4993551e12e405ba8496e465b5800f353a63eda5
znNothing <1067330793@qq.com>
2020-03-19 21:03:26 +0800

    CHANGE 发布之后自动重启nginx

--- 一键发布.bat
+++ 一键发布.bat
@@ -6,4 +6,6 @@ call move %~dp0\public\index1.html %~dp0\public\show
 call move %~dp0\public\show\index.html %~dp0\public
 call rename %~dp0\public\show\index1.html index.html
 call hexo d
+call ssh -v root@47.105.168.3
+call nginx -s reload
 call pause
